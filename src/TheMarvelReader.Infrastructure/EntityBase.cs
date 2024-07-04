0afc2f935d1a08eacb77b93ddc1ca779a8fa5553
Jonathan Raya Rios <jonathan17699@hotmail.es>
2024-07-04 19:23:59 +0200

    Added base for all entities

---
+++ src/TheMarvelReader.Infrastructure/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace TheMarvelReader.Infrastructure
+{
+    public abstract class EntityBase
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+    }
+}
